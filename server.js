dd91b55258d38a66e34cef5030a5060be3e288f1
MDC <markdcross@gmail.com>
2020-12-08 10:42:15 -0500

    Server turns on

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-// Dependencies
+//* =================================
+//* Dependencies
+//* =================================
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
+//* =================================
+//* EJS
+//* =================================
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
 // index page
 app.get('/', function (req, res) {
   res.render('pages/index');
 });
-
 // Portfolio page
 app.get('/portfolio', function (req, res) {
   res.render('pages/portfolio');
 });
 
-app.listen(4040);
-console.log(`https://localhost:${PORT} is the magic port`);
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 4040;
+
+app.listen(PORT, console.log(`http://localhost:${PORT} is the magic port`));
